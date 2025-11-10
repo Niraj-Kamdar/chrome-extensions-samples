bcc1add77fca74bb038f20af5988eb74265024e4
Niraj Kamdar <nirajkamdar27@pm.me>
2025-11-10 14:36:52 +0400

    fix: remove redundant options

--- vite-chrome-extension/src/background.ts
+++ vite-chrome-extension/src/background.ts
@@ -1,11 +1,3 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "options",
-    title: "Extension Options",
-    contexts: ["action"]
-  });
-});
-
 chrome.contextMenus.onClicked.addListener((info) => {
   if (info.menuItemId === "options") {
     chrome.runtime.openOptionsPage();
